d1aa8f2f294a81b8f84a9c6cada402c45daa7453
vladimir-koloski <v.koloski1@gmail.com>
2021-03-29 15:36:19 +0200

    code added

--- femiliFarm-client/src/app/components/product/product.component.ts
+++ femiliFarm-client/src/app/components/product/product.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/auth/auth.service';
+import { CartRequestModel } from 'src/app/models/cart-model';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-product',
@@ -9,13 +11,29 @@ import { AuthService } from 'src/app/auth/auth.service';
 export class ProductComponent implements OnInit {
 
   @Input() product: any;
+  showMessage: boolean = false;
 
   isLogged: boolean = false;
-  constructor(private authService: AuthService){}
+  constructor(private authService: AuthService,
+              private cartService: CartService){}
 
   ngOnInit(){
     this.authService.isLoggedIn.subscribe(isLogged =>{
       this.isLogged = isLogged
     })
+  }
+
+  updateCart(product){
+    let userId = this.authService.getUserId()
+    let request = new CartRequestModel();
+    request = {
+      userId: parseInt(userId),
+      product: product
+    }
+
+    this.cartService.updateCart(request).subscribe({
+      error: err => console.warn(err.error)
+    })
+    this.showMessage = true;    
   }
 }
